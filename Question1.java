268a62a6e7ab3de1c5eaa3e0dda349e90bff28cd
Jingquan <2101268@sit.singaporetech.edu.sg>
2022-01-18 02:38:56 +0800

    Initial Commit

---
+++ Question1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+
+public class Question1 {
+    static final double PI=3.14159;
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number for radius: ");
+        double radius = input.nextDouble();
+        double area = PI * Math.pow(radius, 2);
+        System.out.println("The area for the circle of radius " + radius +" is " + area);
+        input.close();
+
+    }
+}
