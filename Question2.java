04b1027c4157341766daf92f7eb45ca915d36e41
Jingquan <2101268@sit.singaporetech.edu.sg>
2022-01-24 17:50:06 +0800

    upload

---
+++ Question2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        double[] numbers = new double[3];
+        double ans = 0;
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = input.nextDouble();
+            ans += numbers[i]/3;
+        }
+        System.out.println("The average of " + print(numbers) + "is " + ans);
+
+        input.close();
+    }
+
+    static String print(double[] array){
+        String str1 = "";
+        for (int i = 0; i < array.length; i++) {
+            str1 += array[i] + " ";
+        }
+        return str1;
+    }
+}
---
+++ Question3.java
@@ -0,0 +1,12 @@
+public class Question3 {
+    public static void main(String[] args){
+        long millis = System.currentTimeMillis();
+        long TotalSec = millis/1000;
+        long CurrentSec = TotalSec % 60;
+        long TotalMin = TotalSec / 60;
+        long CurrentMin = TotalMin % 60;
+        long TotalHrs = TotalMin / 60;
+        long CurrentHrs = TotalHrs % 24;
+        System.out.println("Current time is " + CurrentHrs + ":" + CurrentMin + ":" + CurrentSec + " GMT");
+    }
+}
